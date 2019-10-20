ad9259fd9a531d14ae6ea346cb45a85251fb16fd
Pawel Rudzki <pawelrudzki89@gmail.com>
2019-10-20 11:11:57 +0200

    Add class: Customer

--- src/main/java/LibraryBook.java
+++ src/main/java/LibraryBook.java
@@ -1,4 +1,16 @@
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Setter
+@Getter
 public class LibraryBook {
 
     private Book book;
+    private int singleBorrowingDuration;
+    private Date borrrowDate;
+    private Customer borrowedBy;
+
+
 }
