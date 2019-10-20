0e170f49ccb8fd35106253816eb013f65180348a
Pawel Rudzki <pawelrudzki89@gmail.com>
2019-10-20 14:47:19 +0200

    LibraryTest: lendBookCustomerOweOverHunderd

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,6 +1,9 @@
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
@@ -45,8 +48,9 @@ public class LibraryTest {
         //then
         assertEquals(5, library.getLibraryWarehouse().size());
     }
+
     @Test
-    public void removeBook(){
+    public void removeBook() {
 //given
         BooksContainer booksContainer = new BooksContainer();
         Library library = new Library(new ArrayList<>(), new ArrayList<>());
@@ -64,8 +68,9 @@ public class LibraryTest {
         assertEquals(3, library.getLibraryWarehouse().size());
 
     }
-    @Test (expected = IllegalStateException.class)
-    public void removeBookExeption(){
+
+    @Test(expected = IllegalStateException.class)
+    public void removeBookExeption() {
         //given
         BooksContainer booksContainer = new BooksContainer();
         Library library = new Library(new ArrayList<>(), new ArrayList<>());
@@ -89,15 +94,16 @@ public class LibraryTest {
     public void addCustomer() {
 
         //given
-        BooksContainer  booksContainer = new BooksContainer();
+        BooksContainer booksContainer = new BooksContainer();
         Customer customer = booksContainer.getCustomer();
         Library library = new Library(new ArrayList<>(), new ArrayList<>());
         //when
     }
+
     @Test
-    public void removeCustomer(){
+    public void removeCustomer() {
         //given
-        BooksContainer  booksContainer = new BooksContainer();
+        BooksContainer booksContainer = new BooksContainer();
         Customer customer = booksContainer.getCustomer();
         Library library = new Library(new ArrayList<>(), new ArrayList<>());
 
@@ -110,6 +116,63 @@ public class LibraryTest {
 
     }
 
+    @Test(expected = IllegalStateException.class)
+    public void lendBookCustomerOverHundredDebt() {
+
+        //given
+        BooksContainer booksContainer = new BooksContainer();
+
+        Customer customer = booksContainer.getCustomer();
+
+        Library library = new Library(new ArrayList<>(), new ArrayList<>());
+        library.addBook(booksContainer.getLibraryBook());
+        library.addBook(booksContainer.getLibraryBook());
+        library.addBook(booksContainer.getLibraryBook());
+        library.addBook(booksContainer.getLibraryBook());
+        library.addBook(booksContainer.getLibraryBook());
+        library.addBook(booksContainer.getLibraryBook());
+        customer.borrowBook(library, library.getLibraryWarehouse().get(0));
+        customer.borrowBook(library, library.getLibraryWarehouse().get(1));
+        customer.borrowBook(library, library.getLibraryWarehouse().get(2));
+
+
+        //when
+
+        customer.setAccountBalance(120);
+        customer.borrowBook(library, library.getLibraryWarehouse().get(3));
+
+
+        //then
+
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void customerAccountOverTime() throws ParseException {
+
+        //given
+        BooksContainer booksContainer = new BooksContainer();
+
+        Customer customer = booksContainer.getCustomer();
+
+        Library library = new Library(new ArrayList<>(), new ArrayList<>());
+        library.addBook(booksContainer.getLibraryBook());
+        library.addBook(booksContainer.getLibraryBook());
+        library.addBook(booksContainer.getLibraryBook());
+        library.addBook(booksContainer.getLibraryBook());
+        customer.borrowBook(library, library.getLibraryWarehouse().get(0));
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        //when
+        library.getLibraryWarehouse().get(0).setBorrrowDate(simpleDateFormat.parse("2019-01-22"));
+        customer.giveBackBook(library, customer.getBooksBorrowedList().get(0));
+        customer.borrowBook(library, library.getLibraryWarehouse().get(2));
+
+
+        //then
+        assertEquals(100, (int) customer.getAccountBalance());
+
+    }
 
 
     @Test
