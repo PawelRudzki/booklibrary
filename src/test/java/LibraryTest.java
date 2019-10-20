288518b336990cff65c949f090fce5b0b45ec618
Pawel Rudzki <pawelrudzki89@gmail.com>
2019-10-20 13:50:11 +0200

    Library: lendBook() with conditions;

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,32 +1,48 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class LibraryTest {
-/*
+
     @Test
     public void acceptBook() {
 
         //given
         BooksContainer booksContainer = new BooksContainer();
-        Library library = new Library();
+
+        Customer customer = booksContainer.getCustomer();
+
+        Library library = new Library(new ArrayList<>(), new ArrayList<>());
+        library.addBook(booksContainer.getLibraryBook());
 
         //when
+        customer.giveBackBook(library, customer.getBooksBorrowedList().get(0));
 
         //then
+        assertEquals(1, customer.getBooksBorrowedList().size());
 
 
     }
 
- */
-
     @Test
     public void addBook() {
 
-        //given
-        //when
-        //then
 
+        //given
+        BooksContainer booksContainer = new BooksContainer();
+        Library library = new Library(new ArrayList<>(), new ArrayList<>());
+
+        //when
+        library.addBook(booksContainer.getLibraryBook());
+        library.addBook(booksContainer.getLibraryBook());
+        library.addBook(booksContainer.getLibraryBook());
+        library.addBook(booksContainer.getLibraryBook());
+        library.addBook(booksContainer.getLibraryBook());
+
+        //then
+        assertEquals(5, library.getLibraryWarehouse().size());
     }
 
     @Test
